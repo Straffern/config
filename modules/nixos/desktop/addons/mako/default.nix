3a4a90ed820c9dd327aa5a50e6aba5fc666fe14a
Alexander Flensborg <14233825+Straffern@users.noreply.github.com>
2024-06-07 14:27:22 +0200

    Improved notification

--- modules/nixos/desktop/addons/mako/default.nix
+++ modules/nixos/desktop/addons/mako/default.nix
@@ -23,13 +23,50 @@ in {
 
     home.configFile."mako/config" = {
       text = ''
+        # GLOBAL CONFIGURATION OPTIONS
+        max-history=100
+        sort=-time
+        
+        # STYLE OPTIONS
+        font=JetBrains Mono 10
+        width=300
+        height=100
+        margin=10
+        padding=15
+        border-size=2
+        border-radius=0
+        icons=1
+        max-icon-size=48
+        icon-location=left
+        markup=1
+        actions=1
+        history=1
+        text-alignment=left
+        default-timeout=5000
+        ignore-timeout=0
+        max-visible=5
+        layer=overlay
+        anchor=top-right
+
         background-color=#${palette.base00}
         text-color=#${palette.base05}
         border-color=#${palette.base0D}
         progress-color=over #${palette.base02}
 
+        
+        [urgency=low]
+        border-color=#${palette.base0D}
+        default-timeout=2000
+
+        [urgency=normal]
+        border-color=#${palette.base0D}
+        default-timeout=5000
+
         [urgency=high]
         border-color=#${palette.base09}
+        text-color=#${palette.base05}
+        default-timeout=0
+
       '';
       onChange = ''
         ${pkgs.busybox}/bin/pkill -SIGUSR2 mako
