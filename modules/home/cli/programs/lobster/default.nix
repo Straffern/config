791393a929448629c95b1b93c284e4208faf812d
Alexander Flensborg <alex@flensborg.dev>
2025-03-06 20:02:31 +0100

    tried to fix lobster

--- modules/home/cli/programs/lobster/default.nix
+++ modules/home/cli/programs/lobster/default.nix
@@ -8,6 +8,27 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ inputs.lobster.packages.${pkgs.system}.default ];
+    home.packages =
+      [ inputs.lobster.packages.${pkgs.system}.default pkgs.ueberzugpp ];
+
+    # use_ueberzugpp=true
+    xdg.configFile."lobster/lobster_config.sh" = {
+      executable = true;
+      text = ''
+        #!/usr/bin/env bash
+
+        preview_window_size=50%
+        ueberzug_x=$(($(tput cols) - 70))
+        ueberzug_y=$(($(tput lines) / 10))
+        ueberzug_max_width=100
+        ueberzug_max_height=100
+        ueberzug_output=sixel
+
+        history=1
+
+        image_preview=1
+        download_dir=~/Videos
+      '';
+    };
   };
 }
