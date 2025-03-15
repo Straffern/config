26d61e0c410d22def82d33481e9c0545ec8e2ff5
Alexander Flensborg <alex@flensborg.dev>
2025-03-15 21:07:23 +0100

    removed templates

--- templates/module/default.nix
+++
@@ -1,9 +0,0 @@
-{ options, config, lib, pkgs, namespace, ... }:
-with lib;
-with lib.${namespace};
-let cfg = config.${namespace}.module;
-in {
-  options.${namespace}.module = { enable = mkEnableOption "Module"; };
-
-  config = mkIf cfg.enable { };
-}
