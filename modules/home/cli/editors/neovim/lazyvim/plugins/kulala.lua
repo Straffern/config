df3b07fe9bdc42494be510285941a9ea7f3f3942
Alexander Flensborg <alex@flensborg.dev>
2025-03-25 08:03:52 +0100

    fixed kulala

--- modules/home/cli/editors/neovim/lazyvim/plugins/kulala.lua
+++ modules/home/cli/editors/neovim/lazyvim/plugins/kulala.lua
@@ -1,19 +1,15 @@
 return {
 	{
-		require("lazy").setup({
-			{
-				"mistweaverco/kulala.nvim",
-				keys = {
-					{ "<leader>Rs", desc = "Send request" },
-					{ "<leader>Ra", desc = "Send all requests" },
-					{ "<leader>Rb", desc = "Open scratchpad" },
-				},
-				ft = { "http", "rest" },
-				opts = {
-					-- your configuration comes here
-					global_keymaps = false,
-				},
-			},
-		}),
+		"mistweaverco/kulala.nvim",
+		keys = {
+			{ "<leader>Rs", desc = "Send request" },
+			{ "<leader>Ra", desc = "Send all requests" },
+			{ "<leader>Rb", desc = "Open scratchpad" },
+		},
+		ft = { "http", "rest" },
+		opts = {
+			-- your configuration comes here
+			global_keymaps = true,
+		},
 	},
 }
