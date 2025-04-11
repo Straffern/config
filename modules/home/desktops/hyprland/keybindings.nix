5c11da32c618c5f2eaa44b751c0374162033d405
Alexander Flensborg <alex@flensborg.dev>
2025-04-11 10:45:02 +0200

    temporary fix for laptop_lid_switch

--- modules/home/desktops/hyprland/keybindings.nix
+++ modules/home/desktops/hyprland/keybindings.nix
@@ -5,8 +5,8 @@ let
   laptop_lid_switch = pkgs.writeShellScriptBin "laptop_lid_switch" ''
     #!/usr/bin/env bash
 
-    if grep open /proc/acpi/button/lid/LID/state; then
-    		hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x0, 1"
+    if grep open /proc/acpi/button/lid/LID/state || grep open /proc/acpi/button/lid/LID[0-9]/state ; then
+    		hyprctl keyword monitor "eDP-1, preffered, 0x0, 1.5"
     else
     		if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
     				hyprctl keyword monitor "eDP-1, disable"
