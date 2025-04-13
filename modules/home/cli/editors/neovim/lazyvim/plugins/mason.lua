c949cc9e7e826cf10839b4de258898ce611372cd
Alexander Flensborg <alex@flensborg.dev>
2025-04-13 16:06:33 +0200

    added extra mason registry. disabled kulala plugin

---
+++ modules/home/cli/editors/neovim/lazyvim/plugins/mason.lua
@@ -0,0 +1,9 @@
+return {
+	"williamboman/mason.nvim",
+	opts = {
+		registries = {
+			"github:mason-org/mason-registry", -- Default registry
+			"github:mimikun/mason-registry", -- Your extra registry (replace <user>/<repo>)
+		},
+	},
+}
