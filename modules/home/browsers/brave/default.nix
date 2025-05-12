a355560ddf9fbe20aca1fede9d65b0eb72e91389
Alexander Flensborg <alex@flensborg.dev>
2025-05-12 21:47:52 +0200

    fixed brave

--- modules/home/browsers/brave/default.nix
+++ modules/home/browsers/brave/default.nix
@@ -7,12 +7,19 @@ in {
   options.${namespace}.browsers.brave = { enable = mkEnableOption "Brave"; };
 
   config = mkIf cfg.enable {
+
+    home.sessionVariables.KDE_USE_SESSION_KEYRING = "0";
+
     programs.brave = {
       enable = true;
       commandLineArgs = [
+        "--disable-features=PasswordManager"
+        "--password-store=basic"
         "--no-default-browser-check"
         "--restore-last-session"
         "--enable-features=TouchpadOverscrollHistoryNavigation"
+        "--enable-features=UseOzonePlatform"
+        "--ozone-platform=wayland"
       ];
     };
     home.persistence."/persist/home/${config.home.username}".directories =
