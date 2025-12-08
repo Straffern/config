0a0976c0b3fced00cb1f16084947ee18b1f5bedd
Alexander Flensborg <alex@flensborg.dev>
2025-12-08 12:54:42 +0100

    enableZshIntegration: true for nix-index

--- modules/home/cli/programs/nix-index/default.nix
+++ modules/home/cli/programs/nix-index/default.nix
@@ -13,6 +13,7 @@ in {
     programs.nix-index = {
       enable = true;
       enableBashIntegration = true;
+      enableZshIntegration = true;
     };
     programs.nix-index-database.comma.enable = true;
   };
