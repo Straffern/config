113c7e7db839957c638ce081a1a9d8eb3eed9077
Alexander Flensborg <alex@flensborg.dev>
2026-01-03 12:28:15 +0100

    fix: update hyprland window rules for version 0.53.0 breaking changes

--- modules/home/desktops/hyprland/windowrules.nix
+++ modules/home/desktops/hyprland/windowrules.nix
@@ -5,61 +5,60 @@ let
 in {
   config = mkIf cfg.enable {
     wayland.windowManager.hyprland.settings = {
-      # New windowrule syntax (Hyprland 0.45+)
-      # Format: windowrule = RULE [params], MATCH_CONDITIONS
+      # New windowrule syntax (Hyprland 0.53+)
+      # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
       windowrule = [
-        # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
-        "suppressevent maximize, class:.*"
+        "suppress_event maximize, match:class .*"
 
         # Just dash of opacity by default
-        "opacity 0.97 0.9, class:.*"
+        "opacity 0.97 0.9, match:class .*"
 
         # Fix some dragging issues with XWayland
-        "nofocus, class:^$, title:^$, xwayland:1, floating:1, fullscreen:0, pinned:0"
+        "no_focus on, match:class ^$, match:title ^$, match:xwayland true, match:float true, match:fullscreen false, match:pin false"
 
         # Float and center file pickers
-        "tag +picker, class:(xdg-desktop-portal-gtk|sublime_text)"
-        "float, tag:picker, title:^(Open.*Files?|Save.*Files?|All Files|Save)"
-        "center, tag:picker, title:^(Open.*Files?|Save.*Files?|All Files|Save)"
-        "size 800 600, tag:picker"
+        "tag +picker, match:class (xdg-desktop-portal-gtk|sublime_text)"
+        "float on, match:tag picker, match:title ^(Open.*Files?|Save.*Files?|All Files|Save)"
+        "center on, match:tag picker, match:title ^(Open.*Files?|Save.*Files?|All Files|Save)"
+        "size 800 600, match:tag picker"
 
         # No transparency on media windows
-        "opacity 1 1, class:^(zoom|vlc|mpv|org.kde.kdenlive|com.obsproject.Studio|com.github.PintaProject.Pinta|imv|org.gnome.NautilusPreviewer)$"
+        "opacity 1 1, match:class ^(zoom|vlc|mpv|org.kde.kdenlive|com.obsproject.Studio|com.github.PintaProject.Pinta|imv|org.gnome.NautilusPreviewer)$"
 
         # Pipewire Volume Control
-        "float, class:^(com.saivert.pwvucontrol)$"
-        "center, class:^(com.saivert.pwvucontrol)$"
-        "size 800 600, class:^(com.saivert.pwvucontrol)$"
+        "float on, match:class ^(com.saivert.pwvucontrol)$"
+        "center on, match:class ^(com.saivert.pwvucontrol)$"
+        "size 800 600, match:class ^(com.saivert.pwvucontrol)$"
 
         # Bluetooth Manager
-        "float, class:^(.blueman-manager-wrapped)$"
-        "center, class:^(.blueman-manager-wrapped)$"
-        "size 800 600, class:^(.blueman-manager-wrapped)$"
+        "float on, match:class ^(.blueman-manager-wrapped)$"
+        "center on, match:class ^(.blueman-manager-wrapped)$"
+        "size 800 600, match:class ^(.blueman-manager-wrapped)$"
 
         # Trayscale
-        "float, class:^(dev.deedles.Trayscale)$"
-        "center, class:^(dev.deedles.Trayscale)$"
-        "size 800 600, class:^(dev.deedles.Trayscale)$"
+        "float on, match:class ^(dev.deedles.Trayscale)$"
+        "center on, match:class ^(dev.deedles.Trayscale)$"
+        "size 800 600, match:class ^(dev.deedles.Trayscale)$"
 
         # Picture-in-picture overlays
-        "tag +pip, title:(Picture.?in.?[Pp]icture)"
-        "float, tag:pip"
-        "pin, tag:pip"
-        "size 600 338, tag:pip"
-        "keepaspectratio, tag:pip"
-        "noborder, tag:pip"
-        "opacity 1 1, tag:pip"
-        "move 100%-w-40 4%, tag:pip"
+        "tag +pip, match:title (Picture.?in.?[Pp]icture)"
+        "float on, match:tag pip"
+        "pin on, match:tag pip"
+        "size 600 338, match:tag pip"
+        "keep_aspect_ratio on, match:tag pip"
+        "decorate false, match:tag pip"
+        "opacity 1 1, match:tag pip"
+        "move 100%-w-40 4%, match:tag pip"
 
         # Force chromium and brave into a tile to deal with --app bug
-        "tile, class:^(Chromium|brave-browser)$"
+        "tile on, match:class ^(Chromium|brave-browser)$"
 
         # Only slight opacity when unfocused
-        "opacity 1 0.97, class:^(Chromium|chromium|google-chrome|google-chrome-unstable|brave-browser)$"
-        "opacity 1 1, initialTitle:^(youtube.com_/)$"
+        "opacity 1 0.97, match:class ^(Chromium|chromium|google-chrome|google-chrome-unstable|brave-browser)$"
+        "opacity 1 1, match:initial_title ^(youtube.com_/)$"
 
-        "idleinhibit fullscreen, class:^(firefox)$"
-        "idleinhibit fullscreen, class:^(brave)$"
+        "idle_inhibit fullscreen, match:class ^(firefox)$"
+        "idle_inhibit fullscreen, match:class ^(brave)$"
       ];
     };
 
