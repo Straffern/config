9ab16eb75b48e6e131291837a9e9409d23366392
Alexander Flensborg <alex@flensborg.dev>
2026-01-05 09:06:11 +0100

    chore(tailscale): suppress health check log spam

--- modules/nixos/services/tailscale/default.nix
+++ modules/nixos/services/tailscale/default.nix
@@ -10,6 +10,15 @@ in {
   config = mkIf cfg.enable {
     services.tailscale.enable = true;
 
+    # Suppress frequent health check "ok" logs
+    # See: https://github.com/tailscale/tailscale/issues/13650
+    systemd.services.tailscaled.environment = {
+      TS_DEBUG_HEALTH_LOGS = "false";
+    };
+
+    # Further reduce noise by setting log level to notice (hides INFO/6 logs)
+    systemd.services.tailscaled.serviceConfig.LogLevelMax = "notice";
+
     # Persist node keys and authentication state
     ${namespace}.system.impermanence.directories = [ "/var/lib/tailscale" ];
   };
