851489e848f1cb3c2dd53a7a8c34768f4ffa83b7
Alexander Flensborg <alex@flensborg.dev>
2026-01-09 11:38:34 +0100

    update cass to 0.1.55 and enable system onnxruntime

--- packages/cass/default.nix
+++ packages/cass/default.nix
@@ -1,24 +1,33 @@
-{ lib, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub, pkg-config, openssl, onnxruntime, lld }:
 
 rustPlatform.buildRustPackage {
   pname = "cass";
-  version = "0.1.36";
+  version = "0.1.55";
 
   src = fetchFromGitHub {
     owner = "Dicklesworthstone";
     repo = "coding_agent_session_search";
-    rev = "v0.1.36";
-    hash = "sha256-plIS1IiMsvh2Mts5JmqRwuD+OTKP/BF7LGoSNnpHw6o=";
+    rev = "v0.1.55";
+    hash = "sha256-T7UjwSetD8PgmSOUII9+gE0JQBJnH6PC2ay+2LoXzNQ=";
   };
 
-  cargoHash = "sha256-tt6RKBgCxOX33fOyPey8U8tlrgxvHOd9tc5lvlbqW84=";
-  
+  cargoHash = "sha256-d+KR0IA1Yca0XPorf8B4QWmesmChCJ55aQny7JDc6XM=";
+
+  nativeBuildInputs = [ pkg-config lld ];
+  buildInputs = [ openssl onnxruntime ];
+
+  env = {
+    ORT_STRATEGY = "system";
+    ORT_LIB_LOCATION = "${onnxruntime}/lib";
+  };
+
   # Disable tests for faster builds (following jj-starship pattern)
   doCheck = false;
 
   meta = with lib; {
     description = "Unified TUI search over local coding agent histories";
-    homepage = "https://github.com/Dicklesworthstone/coding_agent_session_search";
+    homepage =
+      "https://github.com/Dicklesworthstone/coding_agent_session_search";
     license = licenses.mit;
     maintainers = [ ];
     platforms = platforms.all;
