ee33d5bc7edd04148fa06f85a3b39858360a778e
Alexander Flensborg <alex@flensborg.dev>
2026-01-13 09:54:35 +0100

    update jj-ryu to latest commit and drop upstreamed patch

--- packages/jj-ryu/default.nix
+++ packages/jj-ryu/default.nix
@@ -1,12 +1,12 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-, pkg-config
-, openssl
-, stdenv
-, darwin
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  stdenv,
+  darwin,
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "jj-ryu";
   version = "0.0.1-alpha.9";
@@ -14,26 +14,20 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "dmmulroy";
     repo = "jj-ryu";
-    rev = "v${version}";
-    hash = "sha256-eV850bPWkqMFzHxqflKEA1ZgTajy49F3szh9pk9PH+4=";
+    rev = "f4266e2e67cd34e50c552709f87e1506ad27e278";
+    hash = "sha256-jafGP3gseSTHI20TqWsbTKLxqNKIpamopwA+0hQtnSs=";
   };
 
   cargoHash = "sha256-1lexTIKR0QCqFeblkGxf18zCSklYMdtHbfmm2hrdK88=";
 
-  patches = [
-    ./resolve-workspace-root.patch
-  ];
+  nativeBuildInputs = [pkg-config];
 
-  nativeBuildInputs = [
-    pkg-config
-  ];
-
-  buildInputs = [
-    openssl
-  ] ++ lib.optionals stdenv.isDarwin [
-    darwin.apple_sdk.frameworks.Security
-    darwin.apple_sdk.frameworks.SystemConfiguration
-  ];
+  buildInputs =
+    [openssl]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
 
   # Tests require a git/jj environment which is hard to set up in the sandbox
   doCheck = false;
@@ -42,6 +36,6 @@ rustPlatform.buildRustPackage rec {
     description = "Stacked PRs for Jujutsu. Push bookmark stacks to GitHub and GitLab as chained pull requests";
     homepage = "https://github.com/dmmulroy/jj-ryu";
     license = licenses.mit;
-    maintainers = [ ];
+    maintainers = [];
   };
 }
