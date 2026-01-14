a38c0a38a40d820d1ac95a0068f86f7906023f98
Alexander Flensborg <alex@flensborg.dev>
2026-01-14 08:36:51 +0100

    hunk.nvim now uses default config

--- modules/home/cli/editors/neovim/lazyvim/plugins/hunk.lua
+++ modules/home/cli/editors/neovim/lazyvim/plugins/hunk.lua
@@ -4,11 +4,7 @@ return {
 		cmd = { "DiffEditor" },
 		dependencies = { "MunifTanjim/nui.nvim" },
 		config = function()
-			require("hunk").setup({
-				ui = {
-					layout = "horizontal",
-				},
-			})
+			require("hunk").setup()
 		end,
 	},
 }
