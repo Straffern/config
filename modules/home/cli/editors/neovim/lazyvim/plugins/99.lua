c198fd7e69e851be81a053a5d097706bf359ebfb
Alexander Flensborg <alex@flensborg.dev>
2026-01-26 15:03:07 +0100

    add ThePrimeagen/99 neovim AI agent plugin

---
+++ modules/home/cli/editors/neovim/lazyvim/plugins/99.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "ThePrimeagen/99",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      local _99 = require("99")
+      local cwd = vim.uv.cwd()
+      local basename = vim.fs.basename(cwd)
+
+      _99.setup({
+        completion = {
+          source = "cmp",
+        },
+        md_files = {
+          "AGENT.md",
+        },
+      })
+    end,
+    -- stylua: ignore
+    keys = {
+      { "<leader>9f", function() require("99").fill_in_function() end, desc = "99: Fill in function" },
+      { "<leader>9v", function() require("99").visual() end, mode = "v", desc = "99: Visual edit" },
+      { "<leader>9s", function() require("99").stop_all_requests() end, desc = "99: Stop all requests" },
+      { "<leader>9l", function() require("99").view_logs() end, desc = "99: View logs" },
+      { "<leader>9[", function() require("99").prev_request_logs() end, desc = "99: Prev logs" },
+      { "<leader>9]", function() require("99").next_request_logs() end, desc = "99: Next logs" },
+    },
+  },
+}
