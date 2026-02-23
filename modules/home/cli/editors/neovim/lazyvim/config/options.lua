62c7dd21584b7d46c4d33aa732bdc2de38b3c2bf
Alexander Flensborg <alex@flensborg.dev>
2026-02-23 11:26:04 +0100

    fix neovim root folder

--- modules/home/cli/editors/neovim/lazyvim/config/options.lua
+++ modules/home/cli/editors/neovim/lazyvim/config/options.lua
@@ -6,6 +6,8 @@
 vim.g.snacks_animate = false
 vim.g.ai_cmp = false
 
+vim.g.root_spec = { "lsp", { ".jj", "lua" }, { ".git", "lua" }, "cwd" }
+
 vim.api.nvim_create_user_command("HighlightHomoglyphs", function()
 	vim.fn.clearmatches()
 	vim.fn.matchadd("Search", "[^\\x00-\\x7F]", 10)
