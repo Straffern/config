58837c8a788f44e66f50614919b8af91d0290f5d
Alexander Flensborg <alex@flensborg.dev>
2026-02-23 10:08:46 +0100

    improve zellij sesh

--- modules/home/cli/multiplexers/zellij/default.nix
+++ modules/home/cli/multiplexers/zellij/default.nix
@@ -28,31 +28,31 @@
   # };
 
   sesh = pkgs.writeScriptBin "sesh" ''
-    #! /usr/bin/env sh
+    #!/usr/bin/env sh
 
     # Taken from https://github.com/zellij-org/zellij/issues/884#issuecomment-1851136980
     # select a directory using zoxide
     ZOXIDE_RESULT=$(zoxide query --interactive)
     # checks whether a directory has been selected
-    if [[ -z "$ZOXIDE_RESULT" ]]; then
-    	# if there was no directory, select returns without executing
-    	exit 0
+    if [ -z "$ZOXIDE_RESULT" ]; then
+      # if there was no directory, select returns without executing
+      exit 0
     fi
     # extracts the directory name from the absolute path
-    SESSION_TITLE=$(echo "$ZOXIDE_RESULT" | sed 's#.*/##')
+    SESSION_TITLE=$(basename "$ZOXIDE_RESULT")
 
     # get the list of sessions
-    SESSION_LIST=$(zellij list-sessions -n | awk '{print $1}')
+    SESSION_LIST=$(zellij list-sessions -s)
 
     # checks if SESSION_TITLE is in the session list
-    if echo "$SESSION_LIST" | grep -q "^$SESSION_TITLE$"; then
-    	# if so, attach to existing session
-    	zellij attach "$SESSION_TITLE"
+    if printf '%s\n' "$SESSION_LIST" | grep -Fxq "$SESSION_TITLE"; then
+      # if so, attach to existing session
+      zellij attach "$SESSION_TITLE"
     else
-    	# if not, create a new session
-    	echo "Creating new session $SESSION_TITLE and CD $ZOXIDE_RESULT"
-    	cd $ZOXIDE_RESULT
-    	zellij attach -c "$SESSION_TITLE"
+      # if not, create a new session
+      echo "Creating new session $SESSION_TITLE and CD $ZOXIDE_RESULT"
+      cd "$ZOXIDE_RESULT" || exit 1
+      zellij attach -c "$SESSION_TITLE"
     fi
   '';
 in {
