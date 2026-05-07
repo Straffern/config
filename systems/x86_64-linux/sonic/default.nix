317df23202a27292908e3b840a3280a9f9e77207
Alexander Flensborg <alex@flensborg.dev>
2026-05-07 19:17:35 +0200

    Set sonic login shell to zsh

--- systems/x86_64-linux/sonic/default.nix
+++ systems/x86_64-linux/sonic/default.nix
@@ -35,6 +35,7 @@ in
       name = "alex";
       initialHashedPassword = "$6$Xzsm8xWpuEtAOgfe$TMvP8XkkM2UHUSCANLq0CSzmsTVWRDaZNsDn1VlOUQ9WmJUROQYbFkQqHDXmqJ5NYTZn2KY3e/LhmgPQA204z1";
       extraGroups = [ "wheel" ];
+      shell = pkgs.zsh;
       extraOptions = {
         uid = 1000;
       };
