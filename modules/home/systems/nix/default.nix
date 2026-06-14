68b2608f46f174e268676910bd66eec9f7f7400d
Alexander Flensborg <alex@flensborg.dev>
2026-06-14 21:32:57 +0200

    remove redundant hm substitutes

--- modules/home/systems/nix/default.nix
+++ modules/home/systems/nix/default.nix
@@ -31,26 +31,13 @@ in {
 
     nix = {
       settings = {
-        trusted-substituters = [
-          "https://cache.nixos.org"
-          "https://nix-community.cachix.org"
+        # Inherit substituters/keys from NixOS + Determinate; only add HM-specific extras.
+        extra-trusted-substituters = [
           "https://numtide.cachix.org?priority=42"
-          "https://devenv.cachix.org"
-          "https://hyprland.cachix.org"
-          "https://niri.cachix.org"
-          "https://attic.xuyh0120.win/lantian?priority=45"
-          "https://cache.numtide.com?priority=46"
         ];
 
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        extra-trusted-public-keys = [
           "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-          "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
         ];
 
         experimental-features = [
